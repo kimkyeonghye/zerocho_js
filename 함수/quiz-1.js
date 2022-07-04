00a33b687fd08f2b7f89605ba67ae12fc22674f4
kimkyeonghye <guuhwi@gmail.com>
2022-07-04 20:30:13 +0900

    화살표 함수 퀴즈

---
+++ 함수/quiz-1.js
@@ -0,0 +1,12 @@
+// 매개변수로 x,y,z 를 받아 곱한 값을 반환하는 multiply 함수를 만들어 보세요. 단, 화살표 함수로 만드세요.
+
+let multiply = (x, y, z) => x * y * z;
+
+/*
+위와 동일 - 중괄호 생략 가능
+let multiply = (x, y, z) => {
+     return x * y * z;
+};
+*/
+
+console.log(multiply(1, 2, 3));
