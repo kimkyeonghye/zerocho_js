cdaa4a95816650f93f346531cf8a7cdb68b351a2
kimkyeonghye <guuhwi@gmail.com>
2022-07-28 20:36:50 +0900

    Feat: 가위바위보게임 - 클릭 시 이미지 1초 동안 멈춤

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -26,4 +26,20 @@ const computerHand = () => {
     $computer.style.background = `url(${IMG_URL}) ${xCoord[computerChoice]} 0 / auto 200px`;
 };
 
-setInterval(computerHand, 50);
+let intervalID = setInterval(computerHand, 50);
+
+let clickable = true;
+const clickButton = () => {
+    if (clickable) {
+        clearInterval(intervalID);
+        clickable = false;
+        setTimeout(() => {
+            clickable = true;
+            intervalID = setInterval(computerHand, 50);
+        }, 1000);
+    }
+};
+
+$rock.addEventListener('click', clickButton);
+$paper.addEventListener('click', clickButton);
+$scissors.addEventListener('click', clickButton);
